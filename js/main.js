0279dda3e14169b8bde3ca48fc42a98c1bec4d7d
ferranm99 <ferranm99@gmail.com>
2023-12-09 18:08:59 +0400

    portfolio slider setup

--- js/main.js
+++ js/main.js
@@ -262,3 +262,40 @@
 	});		
 
 })(jQuery);
+
+//api https://swiperjs.com/swiper-api#param-coverflowEffect
+//codepen https://codepen.io/kristen17/pen/poqMegm
+
+var swiper = new Swiper(".swiper", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	coverflowEffect: {
+	  rotate: 0,
+	  stretch: 0,
+	  depth: 100,
+	  modifier: 2.5,
+	},
+	keyboard: {
+	  enabled: true
+	},
+	/*mousewheel: {
+	  thresholdDelta: 70
+	},*/
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	spaceBetween: 30,
+	loop: false,
+	breakpoints: {
+	  640: {
+		slidesPerView: 2
+	  },
+	  1024: {
+		slidesPerView: 3
+	  }
+	}
+  });
+  
+  swiper.slideTo(1, false, false);
